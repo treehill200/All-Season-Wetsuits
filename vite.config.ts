c004c2ccd44d8e750e94f2ce6de0e7ee50b5058b
treehill200 <treehill200@gmail.com>
2026-09-18 19:00:38 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: "/All-Season-Wetsuits/",
   plugins: [react()],
   build: {
     target: 'es2020',
